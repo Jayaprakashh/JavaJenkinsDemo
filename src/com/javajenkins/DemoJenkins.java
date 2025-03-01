496bca33dcb20e2d19c7d4676ad1e93f889796de
Jayaprakashh <jayasoft@gmail.com>
2025-03-01 17:11:50 +0530

    Initial Commit - Java Project with JenkinsFile

---
+++ src/com/javajenkins/DemoJenkins.java
@@ -0,0 +1,10 @@
+package com.javajenkins;
+
+public class DemoJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Jenkins! Running Java with JDK 17 on Windows 11.");
+
+	}
+
+}
